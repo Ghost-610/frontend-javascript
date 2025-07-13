06147038dc7d1619fb3342888459c7cfc45803b0
elorm gershon <alidugershon@gmail.com>
2025-07-13 04:25:43 +0000

    i dont even understand what i did here!!!

---
+++ task_1/main.ts
@@ -0,0 +1,17 @@
+interface teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+
+  [key: string]: any; // allows adding extra properties like contract
+}
+const teacher1: teacher = {
+  firstName: "Elorm",
+  lastName: "Gershon",
+  fullTimeEmployee: true,
+  location: "Accra",
+  contract: false, // extra property not defined in the interface
+};
+
